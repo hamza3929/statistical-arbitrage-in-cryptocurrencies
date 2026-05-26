96a3044ce73e16cffcd24e0e64c7a8c0df605d00
hamza3929 <hamza3929@gmail.com>
2026-05-25 22:19:27 -0400

    comment

---
+++ main.py
@@ -0,0 +1,3 @@
+#quant project
+
+# the basis of my research is to check whether crypto that have real world utilities perform better returns like bitcoin compare to the more speculative coines like memecoins
